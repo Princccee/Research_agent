547844ceece099847f39355a46f621d449cff85f
Prince <princekumaraman7654@gmail.com>
2025-03-18 16:32:22 +0530

    welcome on root path

--- main/urls.py
+++ main/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', welcome),
     path('api/', include('research_agent.urls')),
 ]
